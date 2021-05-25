6c611d3c7729bd9251525555e36898c2eccd5354
freecree <bodya20126@gmail.com>
2021-05-25 10:08:54 +0300

    added SpringBoot, config database connection, but it doesn't work

--- src/main/java/config/SpringConfig.java
+++ src/main/java/config/SpringConfig.java
@@ -1,9 +1,29 @@
 package config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+
+import javax.sql.DataSource;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 @ComponentScan(basePackages = {"controllers", "services", "models", "repository"})
 public class SpringConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUsername("mysqluser");
+        dataSource.setPassword("mysqlpass");
+        dataSource.setUrl(
+                "jdbc:mysql://localhost:3306/myDb?createDatabaseIfNotExist=true");
+
+        return dataSource;
+    }
 }
--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,6 +1,12 @@
 package models;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+
+    @Id
     private int id;
     private String login;
     private String password;
